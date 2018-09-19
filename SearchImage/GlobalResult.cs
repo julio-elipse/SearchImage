263a79333e9dfcf48a6c2ed5b49bdb2e1418a49d
Julio Cesar Silva dos Santos <42353967+julio-elipse@users.noreply.github.com>
2018-09-19 08:31:56 -0300

    Added log methods
    Added a general log method and a log method to use when an exception occurs.

--- SearchImage/GlobalResult.cs
+++ SearchImage/GlobalResult.cs
@@ -1,4 +1,5 @@
-﻿namespace SearchImage
+﻿using System;
+namespace SearchImage
 {
   static class GlobalResult
   {
@@ -6,5 +7,24 @@
     /// Global variable to store the number of references to an image file name found on documentation projects
     /// </summary>
     public static int NumberOfReferences;
+    /// <summary>
+    /// This method logs an error to system console
+    /// and then quits the application
+    /// </summary>
+    /// <param name="p_strMessage">Message to log to system console</param>
+    public static void LogErrorAndQuit(string p_strMessage)
+    {
+      Console.WriteLine(p_strMessage);
+      Environment.Exit(Constants.IMG_ENV_EXIT_FAIL);
+    }
+    /// <summary>
+    /// This method logs a general message
+    /// to system console
+    /// </summary>
+    /// <param name="p_strMessage">Message to log to system console</param>
+    public static void LogGeneralMessage(string p_strMessage)
+    {
+      Console.WriteLine(p_strMessage);
+    }
   }
 }
